fe25c6df21e88a59f916167b3af5c8c783344792
Probnika <problick992@gmail.com>
2026-06-15 00:08:04 +0300

    добавил скрипт и кнопку

---
+++ Assets/ScriptManager.cs
@@ -0,0 +1,10 @@
+using UnityEngine;
+
+public class ButtonHandler : MonoBehaviour
+{
+    // Этот метод будет вызываться при нажатии на кнопку
+    public void ShowMessage()
+    {
+        Debug.Log("Захар гей");
+    }
+}
